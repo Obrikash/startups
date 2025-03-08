c07b4846ed37dd706d6f8366264b4df5342fc85a
Nikita <obrikash@gmail.com>
2025-03-08 12:06:50 +0300

    search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container pattern">
@@ -9,6 +15,8 @@ export default function Home() {
           Выложи стартап, голосуй за стартапы и будь замеченным в соревновании
           по стартапам!
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
